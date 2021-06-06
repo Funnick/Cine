dbb788d7eebd7b4c8d8faa51549fcd3b0329b9e7
Funnick <marhdez1999@gmail.com>
2021-06-06 17:43:36 -0400

    Models Commit

---
+++ Migrations/20210606210715_allModels.cs
@@ -0,0 +1,404 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Cine.Migrations
+{
+    public partial class allModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Movies");
+
+            migrationBuilder.RenameTable(
+                name: "Movies",
+                newName: "Movie");
+
+            migrationBuilder.RenameColumn(
+                name: "MovieID",
+                table: "Movie",
+                newName: "MovieId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Country",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Duration",
+                table: "Movie",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Synopsis",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Movie",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Year",
+                table: "Movie",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie",
+                column: "MovieId");
+
+            migrationBuilder.CreateTable(
+                name: "Cinema",
+                columns: table => new
+                {
+                    CinemaId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    NumberOfSeats = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cinema", x => x.CinemaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Discount",
+                columns: table => new
+                {
+                    DiscountId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Percent = table.Column<decimal>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Discount", x => x.DiscountId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Producer",
+                columns: table => new
+                {
+                    Name = table.Column<string>(type: "TEXT", maxLength: 60, nullable: false),
+                    Country = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    Discriminator = table.Column<string>(type: "TEXT", nullable: false),
+                    Role = table.Column<string>(type: "TEXT", nullable: true),
+                    Actor_MovieId = table.Column<int>(type: "INTEGER", nullable: true),
+                    MovieId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Producer", x => x.Name);
+                    table.ForeignKey(
+                        name: "FK_Producer_Movie_Actor_MovieId",
+                        column: x => x.Actor_MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Producer_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TheaterUser",
+                columns: table => new
+                {
+                    TheaterUserId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", maxLength: 70, nullable: false),
+                    Card = table.Column<string>(type: "TEXT", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TheaterUser", x => x.TheaterUserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Show",
+                columns: table => new
+                {
+                    StartTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Date = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    MovieId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CinemaId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Show", x => x.StartTime);
+                    table.ForeignKey(
+                        name: "FK_Show_Cinema_CinemaId",
+                        column: x => x.CinemaId,
+                        principalTable: "Cinema",
+                        principalColumn: "CinemaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Show_Movie_MovieId",
+                        column: x => x.MovieId,
+                        principalTable: "Movie",
+                        principalColumn: "MovieId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TheaterMember",
+                columns: table => new
+                {
+                    TheaterUserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    Points = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TheaterMember", x => x.TheaterUserId);
+                    table.ForeignKey(
+                        name: "FK_TheaterMember_TheaterUser_TheaterUserId",
+                        column: x => x.TheaterUserId,
+                        principalTable: "TheaterUser",
+                        principalColumn: "TheaterUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiscountShow",
+                columns: table => new
+                {
+                    DiscountId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ShowsStartTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscountShow", x => new { x.DiscountId, x.ShowsStartTime });
+                    table.ForeignKey(
+                        name: "FK_DiscountShow_Discount_DiscountId",
+                        column: x => x.DiscountId,
+                        principalTable: "Discount",
+                        principalColumn: "DiscountId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiscountShow_Show_ShowsStartTime",
+                        column: x => x.ShowsStartTime,
+                        principalTable: "Show",
+                        principalColumn: "StartTime",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ticket",
+                columns: table => new
+                {
+                    TicketId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false),
+                    SeatNumber = table.Column<int>(type: "INTEGER", nullable: false),
+                    TheaterUserId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ShowId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ShowStartTime = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ticket", x => x.TicketId);
+                    table.ForeignKey(
+                        name: "FK_Ticket_Show_ShowStartTime",
+                        column: x => x.ShowStartTime,
+                        principalTable: "Show",
+                        principalColumn: "StartTime",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ticket_TheaterUser_TheaterUserId",
+                        column: x => x.TheaterUserId,
+                        principalTable: "TheaterUser",
+                        principalColumn: "TheaterUserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DiscountTicket",
+                columns: table => new
+                {
+                    DiscountsDiscountId = table.Column<int>(type: "INTEGER", nullable: false),
+                    TicketsTicketId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DiscountTicket", x => new { x.DiscountsDiscountId, x.TicketsTicketId });
+                    table.ForeignKey(
+                        name: "FK_DiscountTicket_Discount_DiscountsDiscountId",
+                        column: x => x.DiscountsDiscountId,
+                        principalTable: "Discount",
+                        principalColumn: "DiscountId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DiscountTicket_Ticket_TicketsTicketId",
+                        column: x => x.TicketsTicketId,
+                        principalTable: "Ticket",
+                        principalColumn: "TicketId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscountShow_ShowsStartTime",
+                table: "DiscountShow",
+                column: "ShowsStartTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DiscountTicket_TicketsTicketId",
+                table: "DiscountTicket",
+                column: "TicketsTicketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producer_Actor_MovieId",
+                table: "Producer",
+                column: "Actor_MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Producer_MovieId",
+                table: "Producer",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Show_CinemaId",
+                table: "Show",
+                column: "CinemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Show_MovieId",
+                table: "Show",
+                column: "MovieId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_ShowStartTime",
+                table: "Ticket",
+                column: "ShowStartTime");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ticket_TheaterUserId",
+                table: "Ticket",
+                column: "TheaterUserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DiscountShow");
+
+            migrationBuilder.DropTable(
+                name: "DiscountTicket");
+
+            migrationBuilder.DropTable(
+                name: "Producer");
+
+            migrationBuilder.DropTable(
+                name: "TheaterMember");
+
+            migrationBuilder.DropTable(
+                name: "Discount");
+
+            migrationBuilder.DropTable(
+                name: "Ticket");
+
+            migrationBuilder.DropTable(
+                name: "Show");
+
+            migrationBuilder.DropTable(
+                name: "TheaterUser");
+
+            migrationBuilder.DropTable(
+                name: "Cinema");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Movie",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Country",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Synopsis",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Movie");
+
+            migrationBuilder.DropColumn(
+                name: "Year",
+                table: "Movie");
+
+            migrationBuilder.RenameTable(
+                name: "Movie",
+                newName: "Movies");
+
+            migrationBuilder.RenameColumn(
+                name: "MovieId",
+                table: "Movies",
+                newName: "MovieID");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Movies",
+                type: "TEXT",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Movies",
+                table: "Movies",
+                column: "MovieID");
+        }
+    }
+}

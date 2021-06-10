35d88d6388aa0df2bf63e7a167f05fbb92436651
sr_Dracarys <jaime43lenin@gmail.com>
2021-06-10 02:21:03 -0400

    login and principal view

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -3,14 +3,29 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using Cine.Models;
+
 
 namespace Cine.Controllers
 {
     public class MainController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Main()
         {
             return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }

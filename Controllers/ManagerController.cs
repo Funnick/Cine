3c54304abfd5e7d9a977bef3eda3a4f8241a217b
sr_Dracarys <jaime43lenin@gmail.com>
2021-06-16 15:31:12 -0400

    esto es lo q esta

--- Controllers/ManagerController.cs
+++ Controllers/ManagerController.cs
@@ -9,14 +9,53 @@ using Cine.Models;
 using Cine.ModelsRepository;
 using Cine.ViewModels;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Http;
 
 namespace Cine.Controllers
 {
     public class ManagerController : Controller
     {
+        private readonly IGetRepository<Show> _showRepository;
+        private readonly IGetRepository<Movie> _movieRepository;
+        private readonly IGetRepository<Cinema> _cinemaRepository;
+        private readonly ITicketRepository _ticketRepository;
+        private readonly IGetRepository<Discount> _discountRepository;
+
+
+        public ManagerController(IGetRepository<Show> showRepository, IGetRepository<Movie> movieRepository,
+            IGetRepository<Cinema> cinemaRepository,
+            ITicketRepository ticketRepository,
+            IGetRepository<Discount> discountRepository)
+        {
+            _ticketRepository = ticketRepository;
+            _showRepository = showRepository;
+            _movieRepository = movieRepository;
+            _cinemaRepository = cinemaRepository;
+            _discountRepository = discountRepository;
+        }
         // GET
         public IActionResult Main()
         {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Main(IFormCollection querys)
+        {
+            int value = int.Parse(querys["value"]);
+            switch (querys["statics_by"])
+            {
+                case "day":
+                    var shows = _showRepository.GetAllObj();
+                    var tickets = _ticketRepository.GetAllObj();
+                    shows = shows.Where(x => x.Date)
+                    break;
+
+
+
+                default:
+                    break;
+            }
+
             return View();
         }
     }
